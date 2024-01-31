11a92634732580c4f53202f986b6655ecb546882
Rober <roberdz30@gmail.com>
2024-01-31 08:06:44 -0300

    first commit

---
+++ landing/src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import App from "./App";
+
+const theme = createTheme();
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
